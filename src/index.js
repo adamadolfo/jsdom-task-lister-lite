5786fe477556ff2a3340fa1cd6c8447894ed8950
Adam <adamadolfo8@gmail.com>
2020-10-07 21:15:17 -0400

    good practice with events

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  getForm()
 });
+
+function getForm() {
+  let formEl = document.querySelector("#create-task-form")
+  formEl.addEventListener('submit', handleSubmit)
+  let button = document.querySelector("#button")
+  button.addEventListener('click', handleDelete)
+}
+
+
+function addToDo(toDo, priority) {
+  let liEle = document.createElement("li")
+  let ulEle = document.querySelector("#tasks")
+  liEle.innerText = toDo
+  if (priority == "high") { 
+    liEle.style.color = "red"
+  } else if (priority == "medium") {
+    liEle.style.color = "yellow"
+  } else {
+    liEle.style.color = "green"
+  }
+  ulEle.appendChild(liEle)
+}
+  
+
+function handleSubmit(e) {
+  e.preventDefault()
+  let input = document.querySelector("#new-task-description").value
+  let priority = document.querySelector("#drop-down").value
+ 
+  addToDo(input, priority)
+}
+
+function handleDelete(e) {
+  e.preventDefault()
+  ulEle = document.querySelector("#tasks")
+  ulEle.lastChild.remove()
+
+}
